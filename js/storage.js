96738b5b808f4c96a682755b796a8bc3e660b965
wangmenglei <15737936518@163.com>
2026-09-07 13:14:11 +0800

    Fix GitHub API CORS by removing disallowed cache headers.

--- js/storage.js
+++ js/storage.js
@@ -143,8 +143,6 @@ function authHeaders(token) {
     Accept: "application/vnd.github+json",
     Authorization: `Bearer ${token}`,
     "X-GitHub-Api-Version": "2022-11-28",
-    "Cache-Control": "no-cache",
-    Pragma: "no-cache",
   };
 }
 
